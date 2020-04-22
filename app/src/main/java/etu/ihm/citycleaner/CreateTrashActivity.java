ff5525c2284475e5bc0d5cb947da7c3dc7149ba8
Antoine FACQ <antoine.facq@gmail.com>
2020-04-22 18:44:31 +0200

    Database connection

--- app/src/main/java/etu/ihm/citycleaner/CreateTrashActivity.java
+++ app/src/main/java/etu/ihm/citycleaner/CreateTrashActivity.java
@@ -1,28 +1,28 @@
 package etu.ihm.citycleaner;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+import etu.ihm.citycleaner.database.TrashManager;
+import etu.ihm.citycleaner.ui.mytrashs.Trash;
 
 public class CreateTrashActivity extends Activity {
 
@@ -30,12 +30,44 @@ public class CreateTrashActivity extends Activity {
     private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
+    LocationManager lm;
+
+    double latitude;
+    double longitude;
+
+    private final LocationListener locationListener = new LocationListener() {
+
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private TrashManager databaseManager = new TrashManager(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trash);
 
+
+        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         this.imageView = (ImageView) this.findViewById(R.id.imageView); // Where a miniature will be displayed
 
@@ -57,6 +89,10 @@ public class CreateTrashActivity extends Activity {
             }
         });
 
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, this.locationListener);
+
+
         Button closeButton = (Button) findViewById(R.id.cancel_button);
         closeButton.setOnClickListener(new View.OnClickListener() {
 
@@ -65,6 +101,28 @@ public class CreateTrashActivity extends Activity {
                 finish();
             }
         });
+
+
+        Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                int radioType = ((RadioGroup) findViewById(R.id.radioType)).getCheckedRadioButtonId();
+                int radioClutter = ((RadioGroup) findViewById(R.id.radioClutter)).getCheckedRadioButtonId();
+
+
+
+                Trash trash = new Trash(6, radioType, radioClutter, latitude, longitude, Calendar.getInstance().getTime().toString(), "https://upload.wikimedia.org/wikipedia/commons/3/33/Pedro_playing_for_Chelsea.jpg");
+
+
+                databaseManager.addTrash(trash);
+
+                databaseManager.close();
+            }
+        });
+
     }
 
 
